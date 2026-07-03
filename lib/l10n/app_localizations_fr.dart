076d9bdf0f4a6fa3d6a5ef6bc5ec63c35b148654
OtterWithInternet <drakzoncode@gmail.com>
2026-07-02 21:13:03 -0400

    - Se actualizar partes del I18n para la Internacionalizacion

---
+++ lib/l10n/app_localizations_fr.dart
@@ -0,0 +1,19 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for French (`fr`).
+class AppLocalizationsFr extends AppLocalizations {
+  AppLocalizationsFr([String locale = 'fr']) : super(locale);
+
+  @override
+  String get homeTitle => 'Accueil';
+
+  @override
+  String get profileTab => 'Profil';
+
+  @override
+  String get leaderboardTab => 'Classement';
+}
---
+++ lib/l10n/app_localizations_zh.dart
@@ -0,0 +1,19 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Chinese (`zh`).
+class AppLocalizationsZh extends AppLocalizations {
+  AppLocalizationsZh([String locale = 'zh']) : super(locale);
+
+  @override
+  String get homeTitle => '首页';
+
+  @override
+  String get profileTab => '个人资料';
+
+  @override
+  String get leaderboardTab => '排行榜';
+}
